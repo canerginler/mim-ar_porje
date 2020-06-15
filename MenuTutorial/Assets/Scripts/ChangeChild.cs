4d21ce044685e3b5e932d96e28d4a9f9bf7ff65a
canerginler <58593109+canerginler@users.noreply.github.com>
2020-06-15 15:34:01 +0300

    Mim'ar Proje Dosyası

---
+++ MenuTutorial/Assets/Scripts/ChangeChild.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mime;
+using System.Threading;
+using UnityEngine;
+
+public class ChangeChild : MonoBehaviour
+{
+    public List<GameObject> itemList;
+    [SerializeField] GameObject ImageTarget;
+    [SerializeField] GameObject ItemsObject;
+    private void Start()
+    {
+        ChangeChildItem(PlayerPrefs.GetInt("ItemId"));
+    }
+    public void ChangeChildItem(int itemId)
+    {
+        ImageTarget.transform.GetChild(0).parent = ItemsObject.transform;
+        itemList[itemId].transform.parent = ImageTarget.transform;
+    }
+}
---
+++ MenuTutorial/Assets/Scripts/main.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.SceneManagement;
+public class main : MonoBehaviour
+{
+    public void sahneGecis(int itemId)
+    {
+        PlayerPrefs.SetInt("ItemId", itemId);
+        SceneManager.LoadScene(1);
+    }
+
+    public void quitButton()
+    {
+        Debug.Log("Quit!!");
+        Application.Quit();
+    }
+}
